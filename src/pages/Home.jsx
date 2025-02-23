669dcc8a0ff808448cb35b672c6a2960362e2d2d
Shuga8 <lotocharles8@gmail.com>
2025-02-23 17:38:20 +0100

    fixing installtion, tailwind css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigation, Hero } from "../components";
+
+const Home = () => {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
