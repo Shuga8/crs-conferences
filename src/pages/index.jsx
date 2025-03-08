52e5fa0bf89bd9e1e7bb96dc652a06dfad13c6ec
Shuga8 <lotocharles8@gmail.com>
2025-03-08 02:03:29 +0100

    navbar, other pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,6 @@
 export { default as Home } from "./Home";
+export { default as CallForPapers } from "./CallForPapers";
+export { default as Shop } from "./Shop";
+export { default as Sponsorship } from "./Sponsorship";
+export { default as Volunteer } from "./Volunteer";
+export { default as Patnership } from "./Patnership";
