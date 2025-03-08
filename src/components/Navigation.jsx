d3156ccc55dbc9d4ee659f5548313b3d6b46e7d8
Shuga8 <lotocharles8@gmail.com>
2025-03-08 02:30:33 +0100

    navbar styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 import logo from "../assets/CRS_logo.svg";
 
 const Navigation = () => {
+  const pathNameCheck = (path) => {
+    return window.location.pathname === path;
+  };
+
+  const pathNameLike = (path) => {
+    return window.location.pathname.includes(path);
+  };
   return (
     <>
       <nav
@@ -20,35 +27,55 @@ const Navigation = () => {
         <div className="nav-links hidden lg:flex flex-row gap-x-6 place-items-center">
           <Link
             to={"/"}
-            className="py-1.5 border-b-2 border-orange-800 text-orange-800 hover:text-orange-700 transition-colors ease-linear duration-300"
+            className={`py-1.5 ${
+              pathNameCheck("/") ? "border-b-2 border-orange-900" : ""
+            } text-orange-900 hover:text-orange-700 transition-colors ease-linear duration-300`}
           >
             Home
           </Link>
           <Link
-            to={"/"}
-            className="py-1.5 text-orange-800 hover:text-orange-700 transition-colors ease-linear duration-300"
+            to={"/call-for-papers"}
+            className={`py-1.5 ${
+              pathNameCheck("/call-for-papers")
+                ? "border-b-2 border-orange-900"
+                : ""
+            } text-orange-900 hover:text-orange-700 transition-colors ease-linear duration-300`}
           >
             Call for Papers
           </Link>
           <div className="dropdown">
-            <div className="dropdown-title cursor-pointer" role="link">
+            <div
+              className={`dropdown-title cursor-pointer ${
+                pathNameCheck("/get-involved")
+                  ? "border-b-2 border-orange-900"
+                  : ""
+              } text-orange-900 hover:text-orange-700 transition-colors ease-linear duration-300`}
+              role="link"
+            >
               Get Involved
             </div>
 
             <div className="dropdown-links">
               <div className="">
-                <Link to={""}>Sponsorship</Link>
-                <Link to={""}>Volunteer</Link>
-                <Link to={""}>Partnership</Link>
+                <Link to={"/get-involved/sponsorship"}>Sponsorship</Link>
+                <Link to={"/get-involved/volunteer"}>Volunteer</Link>
+                <Link to={"/get-involved/patnership"}>Partnership</Link>
               </div>
             </div>
           </div>
-          <Link to={"/"}>Shop</Link>
+          <Link
+            to={"/shop"}
+            className={`py-1.5 ${
+              pathNameCheck("/shop") ? "border-b-2 border-orange-900" : ""
+            } text-orange-900 hover:text-orange-700 transition-colors ease-linear duration-300`}
+          >
+            Shop
+          </Link>
         </div>
 
         <Link
-          to={"/"}
-          className="hidden lg:block text-[12px] font-[450] px-6 py-2 bg-orange-800 border border-orange-800 hover:bg-transparent hover:text-orange-800 text-white transition-colors ease-linear duration-300"
+          to={"/contact"}
+          className="hidden lg:block text-[12px] font-[450] px-6 py-2 bg-orange-900 border border-orange-900 hover:bg-transparent hover:text-orange-900 text-white transition-colors ease-linear duration-300"
         >
           Contact
         </Link>
