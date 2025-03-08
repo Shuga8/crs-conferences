73532012a54bc2c1ede02fb7da8d2aeafbba2064
Shuga8 <lotocharles8@gmail.com>
2025-03-08 02:14:30 +0100

    routes and components structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,36 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages";
+import {
+  CallForPapers,
+  Contact,
+  Forbidden,
+  Home,
+  NotFound,
+  Patnership,
+  ServerError,
+  Shop,
+  Sponsorship,
+  Volunteer,
+} from "./pages";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/call-for-papers" element={<CallForPapers />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/get-involved/sponsorship" element={<Sponsorship />} />
+        <Route path="/get-involved/volunteer" element={<Volunteer />} />
+        <Route path="/get-involved/partnership" element={<Patnership />} />
+
+        {/** Error Routes */}
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/500" element={<ServerError />} />
+        <Route path="/403" element={<Forbidden />} />
+        <Route path="*" element={<NotFound />} />
       </>
     )
   );
